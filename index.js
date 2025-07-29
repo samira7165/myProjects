dcc9f668a4ae4e9ba349a29f7c35c98dfb066736
samira7165 <141204267+samira7165@users.noreply.github.com>
2025-07-30 00:56:30 +0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,26 +1,25 @@
-const dbtn= document.getElementById("b1");
-const rbtn= document.getElementById("b2");
-const ibtn= document.getElementById("b3");
-const countLabel= document.getElementById("contLable");
-let count=0;
+const myButton=document.getElementById("mybutton");
+const myLabel1=document.getElementById("label1");
+const myLabel2=document.getElementById("label2");
+const myLabel3=document.getElementById("label3");
 
 
-ibtn.onclick=function(){
-    count++;
-    countLabel.textContent=count;
+const min=1;
+const max=6;
+let randomNum1;
+let randomNum2;
+let randomNum3;
+
+myButton.onclick=function(){
+    randomNum1=Math.floor(Math.random() * max)+min;
+    randomNum2=Math.floor(Math.random() * max)+min;
+    randomNum3=Math.floor(Math.random() * max)+min;
+    myLabel1.textContent=randomNum1;
+    myLabel2.textContent=randomNum2;
+    myLabel3.textContent=randomNum3;
+
+
+
+
 }
-dbtn.onclick=function(){
-    count--;
-    countLabel.textContent=count;
-}
-
-rbtn.onclick=function(){
-    count=0;
-    countLabel.textContent=count;
-}
-
-
-
-
-
 
